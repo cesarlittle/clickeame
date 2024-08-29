66336ba88678ac97e9db019a85b765caba731194
cesarlittle <cesar.little.p@gmail.com>
2024-08-29 12:30:53 -0300

    listo el Portfolio definitivo

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-//Hello//
+//animacion fade in//
+//document.addEventListener("DOMContentLoaded", function() {
+    //const fadeInElements = document.querySelectorAll('.fade-in-hidden');
+    
+    //function handleScroll() {
+        //fadeInElements.forEach(function(element) {
+            //const position = element.getBoundingClientRect().top;
+            //const windowHeight = window.innerHeight;
+            
+            //if (position < windowHeight - 50) {
+                //element.classList.add('fade-in');
+            //}
+        //});
+    //}
+    
+    //window.addEventListener('scroll', handleScroll);
+    //handleScroll(); // Para animar los elementos visibles al cargar la página//
+
+//});
+
