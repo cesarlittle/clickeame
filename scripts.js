68ccc34752a13f9a02452018d93c89583d0c0e39
cesarlittle <cesar.little.p@gmail.com>
2024-08-30 02:25:09 -0300

    haciemdo responsive

--- scripts.js
+++ scripts.js
@@ -1,20 +1,2 @@
-//animacion fade in//
-//document.addEventListener("DOMContentLoaded", function() {
-    //const fadeInElements = document.querySelectorAll('.fade-in-hidden');
-    
-    //function handleScroll() {
-        //fadeInElements.forEach(function(element) {
-            //const position = element.getBoundingClientRect().top;
-            //const windowHeight = window.innerHeight;
-            
-            //if (position < windowHeight - 50) {
-                //element.classList.add('fade-in');
-            //}
-        //});
-    //}
-    
-    //window.addEventListener('scroll', handleScroll);
-    //handleScroll(); // Para animar los elementos visibles al cargar la página//
 
-//});
 
