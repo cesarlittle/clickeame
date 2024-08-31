0df7041ab2e6589ce42dff0adb85af581c9ca00d
cesarlittle <cesar.little.p@gmail.com>
2024-08-30 22:41:53 -0300

    Todo Listo final Final jaja

--- scripts.js
+++ scripts.js
@@ -6,3 +6,19 @@ window.addEventListener("scroll", function() {
         header.classList.remove("scrolled");
     }
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const menuHamburguesa = document.querySelector(".menu-hamburguesa");
+    const menuItems = document.querySelector(".menu-items");
+
+    menuHamburguesa.addEventListener("click", function() {
+        menuItems.classList.toggle("show");
+    });
+
+    // Cerrar el menú al hacer clic en un enlace
+    const menuLinks = document.querySelectorAll(".menu-items a");
+    menuLinks.forEach(function(link) {
+        link.addEventListener("click", function() {
+            menuItems.classList.remove("show");
+        });
+    });
+});
